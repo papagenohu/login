39e26e00b3fcfd8992135485bda250f5ac985abc
Papageno <it@papageno.com>
2025-06-07 15:08:25 +0200

    Update widget.tsx

--- kindeSrc/components/widget.tsx
+++ kindeSrc/components/widget.tsx
@@ -28,7 +28,11 @@ export const Widget = (props: { heading: string; description: string }) => {
   return (
     <main style={styles.loginForm}>
       <div style={{ padding: "2rem" }}>
-        <img src="papageno-logo_atlatszo.png" alt="Logo" style={{ width: "120px", marginBottom: "1rem" }} />
+        <img
+  src="/papageno-logo_atlatszo.png"
+  alt="Papageno logó"
+  style={{ width: "120px", marginBottom: "1rem" }}
+/>
         <h2 style={styles.heading}>{props.heading}</h2>
         <p style={styles.description}>{props.description}</p>
         {getKindeWidget()}
